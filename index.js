7d8d238cadb0ab3102729cb869e9ab3a9b7de0e3
Nicholas <bestnicholas05@gmail.com>
2024-05-05 14:22:30 -0400

    Completed Functions Lab Cont

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(name = 'roller-skate') {
+    return `This Saturday, I want to ${name}!`
+}
+function mondayWork(name = 'go to the office') {
+    return `This Monday, I will ${name}.`
+}
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+        
+}
